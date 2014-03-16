caea23be4eddfecbbf239e479692c6b44dca4a24
Samuele Santi <redshadow@hackzine.org>
2014-03-16 19:54:17 +0100

    More tests for the parser

--- dq/tests/unit/test_parser.py
+++ dq/tests/unit/test_parser.py
@@ -68,3 +68,59 @@ def test_parsing_with_pyargs(parser):
 
     ## Output('myfile2.csv', sep=',')
     assert isinstance(tree[1], Device)
+
+
+def test_parsing_with_blocks_and_pyargs(parser):
+    text = """
+    { Dev1("foo", name='bar') | Dev2(hello == 'world'),
+      Dev3() | Dev4(example=item + 100) }
+    | Collector(bla="foobar") | Piped('here') | {
+      Out1(), Out2(), Filter() | Out3(),
+    }
+    """
+
+    tree = parser.parse(text)
+    assert isinstance(tree, Pipeline)
+    assert len(tree) == 4
+
+    # { Dev1("foo", name='bar') | Dev2(hello == 'world'),
+    #   Dev3() | Dev4(example=item + 100) }
+    assert isinstance(tree[0], PipelineBlock)
+    assert len(tree[0]) == 2
+
+    # Dev1("foo", name='bar') | Dev2(hello == 'world')
+    assert isinstance(tree[0][0], Pipeline)
+
+    assert isinstance(tree[0][0][0], Device)
+    assert tree[0][0][0].name == 'Dev1'
+    assert len(tree[0][0][0].args) == 1
+    assert len(tree[0][0][0].keywords) == 1
+    assert tree[0][0][0].keywords['name'].evaluate() == 'bar'
+
+    assert isinstance(tree[0][0][1], Device)
+    assert tree[0][0][1].name == 'Dev2'
+    assert tree[0][0][1].keywords == {}
+    assert tree[0][0][1].args[0].evaluate({'hello': 'world'}) is True
+    assert tree[0][0][1].args[0].evaluate({'hello': 'nothing'}) is False
+
+    # Dev3() | Dev4(example=item + 100)
+    assert isinstance(tree[0][1], Pipeline)
+
+    assert isinstance(tree[0][1][0], Device)
+    assert tree[0][1][0].name == 'Dev3'
+    assert tree[0][1][0].args == []
+    assert tree[0][1][0].keywords == {}
+
+    assert isinstance(tree[0][1][1], Device)
+    assert tree[0][1][1].name == 'Dev4'
+    assert tree[0][1][1].args == []
+    assert tree[0][1][1].keywords['example'].evaluate({'item': 10}) == 110
+
+    # Collector(bla="foobar")
+    assert isinstance(tree[1], Device)
+
+    # Piped('here')
+    assert isinstance(tree[2], Device)
+
+    # { Out1(), Out2(), Filter() | Out3() }
+    assert isinstance(tree[3], PipelineBlock)
